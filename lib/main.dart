855b8f088ed1d8775c150c6f68ffe085b7c683dd
richikchanda1999 <richikchanda1999@gmail.com>
2020-01-16 18:17:20 +0530

    Conflicts resolved for yash

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:library_management/UI/login.dart';
-import 'UI/sohan.dart' as sohan;
-
-import 'UI/Manul.dart' as manul;
-import 'UI/Streams.dart' as streams;
-
 class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
